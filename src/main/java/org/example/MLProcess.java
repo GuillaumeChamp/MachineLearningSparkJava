41631c064626a1eb140f32a4528d91506aeec2d6
guillaume <guillaume.champtoussel@ensea.fr>
2022-12-17 19:34:11 +0100

    patch

--- src/main/java/org/example/MLProcess.java
+++ src/main/java/org/example/MLProcess.java
@@ -2,13 +2,13 @@ package org.example;
 
 import org.apache.spark.ml.Pipeline;
 import org.apache.spark.ml.PipelineStage;
-import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator;
 import org.apache.spark.ml.evaluation.RegressionEvaluator;
 import org.apache.spark.ml.feature.Imputer;
 import org.apache.spark.ml.feature.Normalizer;
 import org.apache.spark.ml.feature.VectorAssembler;
 import org.apache.spark.ml.param.ParamMap;
 import org.apache.spark.ml.regression.LinearRegression;
+import org.apache.spark.ml.regression.RandomForestRegressor;
 import org.apache.spark.ml.tuning.CrossValidator;
 import org.apache.spark.ml.tuning.CrossValidatorModel;
 import org.apache.spark.ml.tuning.ParamGridBuilder;
@@ -17,29 +17,35 @@ import org.apache.spark.sql.Row;
 
 public class MLProcess {
     private static final String[] imputedCols = new String[]{"ArrDelay","DepDelay","TaxiOut"};
+    private static final String[] imputedColsO = new String[]{"ArrDelayImputed","DepDelayImputed","TaxiOutImputed"};
     private static final String[] assembled = new String[]{"Month","DayofMonth","DayOfWeek"};
     protected static CrossValidatorModel process(Dataset<Row> cleaned){
-        //Split
-        Dataset<Row>[] split = cleaned.randomSplit(new double[]{0.7,0.3});
-        Dataset<Row> training = split[0];
-        Dataset<Row> test = split[1];
         //create the stages
-        VectorAssembler assembler = new VectorAssembler()
-                .setInputCols(assembled)
-                .setOutputCol("features");
         Imputer imputer = new Imputer()
                 .setStrategy("mean")
                 .setInputCols(imputedCols)
-                .setOutputCols(imputedCols);
-        Normalizer normalizer = new Normalizer(); //p=2
+                .setOutputCols(imputedColsO);
+        Normalizer normalizer = new Normalizer()
+                .setInputCol("features")
+                .setOutputCol("NormalizedFeatures"); //p=2
+        VectorAssembler assembler = new VectorAssembler()
+                .setInputCols(assembled)
+                .setOutputCol("features");
         LinearRegression lr = new LinearRegression()
                 .setMaxIter(10)
                 .setRegParam(0.3)
                 .setElasticNetParam(0.8)
-                .setLabelCol("FlightNum");
+                .setLabelCol("ArrDelay");
+        RandomForestRegressor rm = new RandomForestRegressor()
+                .setLabelCol("prediction");
         //Create the pipeline
         Pipeline pipeline = new Pipeline()
-                .setStages(new PipelineStage[] {assembler,imputer/*,normalizer*/,lr});
+                .setStages(new PipelineStage[] {imputer
+                        ,assembler
+                        ,normalizer
+                        ,lr
+                        //,rm
+                });
         
         //Cross validation
         ParamMap[] paramGrid = new ParamGridBuilder()
@@ -54,10 +60,9 @@ public class MLProcess {
                 .setParallelism(2);  // Evaluate up to 2 parameter settings in parallel
 
         // Run cross-validation, and choose the best set of parameters.
-        CrossValidatorModel cvModel = cv.fit(training);
+        CrossValidatorModel cvModel = cv.fit(cleaned);
         //Use the pipeline
-        //PipelineModel model = pipeline.fit(training);
-        cvModel.transform(test).show();
+        cvModel.transform(cleaned).show();
         return cvModel;
     }
 }
