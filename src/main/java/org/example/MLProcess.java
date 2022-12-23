6e0502c5467e8f80019748c48df94e6e5bb78933
Kairo5 <kairosagewing5@gmail.com>
2022-12-23 16:05:01 +0100

    Forgot to uncomment rm

--- src/main/java/org/example/MLProcess.java
+++ src/main/java/org/example/MLProcess.java
@@ -67,7 +67,7 @@ public class MLProcess {
                         , assembler
                         , normalizer
                         , lr
-                        //, rm
+                        , rm
                 });
 
 
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -26,6 +26,9 @@ public class Main {
 
     //To run add the following arguments Path\To\Documents\BD\1998.csv C:\Path\To\Documents\BD\1998.csv
     public static void main(String[] args) {
+
+        System.out.println("hola");
+
         if (!handleArguments(args)) return;
         SparkConf conf = new SparkConf().setAppName("FlightDelayLearning").set("spark.executor.memory", "8g");
         if (local) conf = conf.setMaster("local[2]");
